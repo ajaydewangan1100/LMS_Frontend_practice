1b5b3a74b8467cc2761c6db743a128e0bf037064
ajaydewangan1100 <ajaydewangan1100@gmail.com>
2024-01-04 13:16:07 +0530

    Delete button added under course list

--- src/Components/CourseCard.jsx
+++ src/Components/CourseCard.jsx
@@ -1,15 +1,47 @@
+import { BsTrash } from "react-icons/bs";
+import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
+import { deleteCourse, getAllCourses } from "../Redux/Slices/CourseSlice";
 
 function CourseCard(data) {
   const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  //   for checking if user is loggedin
+  const isLoggedIn = useSelector((state) => state?.auth?.isLoggedIn);
+
+  //   for displayng options according to role
+  const role = useSelector((state) => state?.auth?.role);
+
   data = data?.data;
+
+  async function onCourseDelete(id, e) {
+    e.stopPropagation();
+    if (window.confirm("Are you sure you want to delete the course?")) {
+      const res = await dispatch(deleteCourse(id));
+
+      if (res?.payload?.success) {
+        await dispatch(getAllCourses());
+      }
+    }
+  }
 
   return (
     <div
-      onClick={() => navigate("/course/description", {state: data})}
+      onClick={() => navigate("/course/description", { state: data })}
       className="text-white w-[22rem] h-[430px] shadow-lg rounded-lg cursor-pointer group overflow-hidden bg-zinc-700 "
     >
-      <div className="overflow-hidden">
+      <div className="overflow-hidden relative">
+        {isLoggedIn && role === "ADMIN" ? (
+          <button
+            className="absolute right-1 bg-red-500 rounded-md top-2 text-white py-1 px-2 border"
+            onClick={(e) => onCourseDelete(data?._id, e)}
+          >
+            <BsTrash />
+          </button>
+        ) : (
+          ""
+        )}
         <img
           className="h-48 w-full rounded-t-lg group-hover:scale-[1,2] transition-all ease-in-out duration-300 "
           src={data?.thumbnail?.secure_url}
